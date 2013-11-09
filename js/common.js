33ca74375c9ebf9d49d9923e856847df946c60d5
Mark <marky04@ymail.com>
2013-11-08 18:35:32 -0800

    test

--- js/common.js
+++ js/common.js
@@ -23,10 +23,9 @@ function confirmBackHome()
 function playMusic()
 {
 	//playback sound
-	var playCompleteAudio = new Audio('sound/background_music.mp3');
+	/*var playCompleteAudio = new Audio('sound/background_music.mp3');
 	playCompleteAudio.currentTime == 0;
-	playCompleteAudio.play();
-	playCompleteAudio.prop('volume', 0.1);
+	playCompleteAudio.play();*/
 }
 
 // Wait for device API libraries to load
@@ -37,6 +36,7 @@ function onDeviceReady()
 {
 	var db = window.openDatabase('memory_game_centre', '1.0', 'Memory Game Local Storage', 200000);
 	successCB();
+	playAudio('sound/background_music.mp3');
 }
 
 // Query the database
@@ -118,4 +118,75 @@ function updateConfigMusic(tx)
 	}
 
 	tx.executeSql('UPDATE config SET config_value = "' + configValue + '" WHERE config_key = "music_tracks"');
+}
+
+// Audio player
+var my_media = null;
+var mediaTimer = null;
+
+// Play audio
+//
+function playAudio(src) {
+	// Create Media object from src
+	my_media = new Media(src, onSuccess, onError);
+
+	// Play audio
+	my_media.play();
+
+	// Update my_media position every second
+	if (mediaTimer == null) {
+		mediaTimer = setInterval(function() {
+			// get my_media position
+			my_media.getCurrentPosition(
+				// success callback
+				function(position) {
+					if (position > -1) {
+						setAudioPosition((position) + " sec");
+					}
+				},
+				// error callback
+				function(e) {
+					console.log("Error getting pos=" + e);
+					setAudioPosition("Error: " + e);
+				}
+			);
+		}, 1000);
+	}
+}
+
+// Pause audio
+//
+function pauseAudio() {
+	if (my_media) {
+		my_media.pause();
+	}
+}
+
+// Stop audio
+//
+function stopAudio() {
+	if (my_media) {
+		my_media.stop();
+	}
+	clearInterval(mediaTimer);
+	mediaTimer = null;
+}
+
+// onSuccess Callback
+//
+function onSuccess() {
+	console.log("playAudio():Audio Success");
+}
+
+// onError Callback
+//
+function onError(error) {
+	alert('code: '    + error.code    + '\n' +
+		  'message: ' + error.message + '\n');
+}
+
+// Set audio position
+//
+function setAudioPosition(position) {
+	document.getElementById('audio_position').innerHTML = position;
 }
