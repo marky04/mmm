d356594d8154ace8c2e53ab648998cd21a20e49f
Mark <marky04@ymail.com>
2013-12-17 17:09:13 -0800

    test

--- js/common.js
+++ js/common.js
@@ -40,10 +40,9 @@ function onPause()
 // Create score_ranks table
 function createTable(tx)
 {//alert('taz create table');
-	tx.executeSql('DROP TABLE IF EXISTS score_ranks');
+	//tx.executeSql('DROP TABLE IF EXISTS score_ranks');
 	//tx.executeSql('CREATE TABLE IF NOT EXISTS score_ranks (name, score, difficulty)');
 	tx.executeSql('CREATE TABLE IF NOT EXISTS score_ranks (id INTEGER PRIMARY KEY AUTOINCREMENT, score INTEGER, difficulty, date_added DATE)');
-	tx.executeSql('INSERT INTO score_ranks (score, difficulty, date_added) VALUES ("21", "easy", "' + new Date().getTime() + '")');
 
 	tx.executeSql('DROP TABLE IF EXISTS config');
 	tx.executeSql('CREATE TABLE IF NOT EXISTS config (config_id unique, config_key, config_value)');
